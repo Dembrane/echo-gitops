012f0201095f5611c27264807b02d82bff829c40
Sameer Pashikanti <sameer.pashikanti@gmail.com>
2025-03-07 14:51:50 +0100

    Refactor Kubernetes and ArgoCD configuration in Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -60,14 +60,25 @@ resource "digitalocean_container_registry_docker_credentials" "registry_credenti
   registry_name = digitalocean_container_registry.registry.name
 }
 
+resource "time_sleep" "wait_for_kubernetes" {
+  depends_on      = [digitalocean_kubernetes_cluster.doks]
+  create_duration = "30s"
+}
+
 resource "kubernetes_namespace" "echo_ns" {
   metadata {
     name = "echo-${var.env}"
   }
+
+  depends_on = [time_sleep.wait_for_kubernetes]
+}
+
+data "digitalocean_kubernetes_cluster" "doks_data" {
+  name = "dbr-echo-${var.env}-k8s-cluster" # Use the same name as your resource
 }
 
 resource "kubernetes_secret" "registry_credentials" {
-  depends_on = [digitalocean_kubernetes_cluster.doks]
+  depends_on = [time_sleep.wait_for_kubernetes]
 
   metadata {
     name      = "do-registry-secret"
@@ -80,3 +91,79 @@ resource "kubernetes_secret" "registry_credentials" {
 
   type = "kubernetes.io/dockerconfigjson"
 }
+
+provider "kubernetes" {
+  host  = data.digitalocean_kubernetes_cluster.doks_data.endpoint
+  token = data.digitalocean_kubernetes_cluster.doks_data.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    data.digitalocean_kubernetes_cluster.doks_data.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.digitalocean_kubernetes_cluster.doks_data.endpoint
+    token                  = data.digitalocean_kubernetes_cluster.doks_data.kube_config[0].token
+    cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.doks_data.kube_config[0].cluster_ca_certificate)
+  }
+}
+
+provider "kubectl" {
+  host                   = data.digitalocean_kubernetes_cluster.doks_data.endpoint
+  token                  = data.digitalocean_kubernetes_cluster.doks_data.kube_config[0].token
+  cluster_ca_certificate = base64decode(data.digitalocean_kubernetes_cluster.doks_data.kube_config[0].cluster_ca_certificate)
+  load_config_file       = false
+}
+
+### ArgoCD
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+
+  depends_on = [time_sleep.wait_for_kubernetes]
+}
+
+# this doesnt work just do 
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# username: admin
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+# kubectl apply -f echo-dev.yaml
+
+# resource "helm_release" "argocd" {
+#   depends_on = [kubernetes_namespace.argocd]
+
+#   name       = "argocd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   version    = "5.51.4" # Specify your desired version
+#   namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+#   # Basic configuration values
+#   set {
+#     name  = "server.service.type"
+#     value = "LoadBalancer" # Or ClusterIP if you plan to use an Ingress
+#   }
+
+#   set {
+#     name  = "server.service.port"
+#     value = "80"
+#   }
+
+#   # Optional: Set admin password 
+#   set_sensitive {
+#     name = "configs.secret.argocdServerAdminPassword"
+#     # This is the bcrypted hash of your password - generate using:
+#     # htpasswd -nbBC 10 "" yourpassword | tr -d ':\n' | sed 's/$2y/$2a/'
+#     value = "$2a$10$hI2iRa4C5AFgG/pAytvKz.e6rXgLUYjL4.GA8HyMIrUEGC/VSBjue"
+#   }
+
+#   # Additional configurations can be set here
+#   values = [
+#     <<-EOT
+#     server:
+#       extraArgs:
+#         - --insecure
+#     EOT
+#   ]
+# }
